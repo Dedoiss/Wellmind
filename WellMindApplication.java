19f777f875976964b87ec1a28705e8d92d75b39f
Dedoiss <162224693+Dedoiss@users.noreply.github.com>
2025-11-06 23:27:47 +0000

    Create WellMindApplication.java

---
+++ WellMindApplication.java
@@ -0,0 +1,11 @@
+package com.wellmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellMindApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WellMindApplication.class, args);
+    }
+}
